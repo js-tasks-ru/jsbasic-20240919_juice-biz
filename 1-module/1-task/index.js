8b09aee525b746f77b1d39df5c78499a05035b31
Ivan Klyushin <juice_biz@mail.ru>
2024-10-06 12:04:52 +0300

    Second homework completed

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,11 @@
 function factorial(n) {
-  // ваш код...
+  if (n === 0) {
+    return 1;
+  } else {
+    let factorial = n;
+    for (let i = n - 1; i > 0; i--) {
+      factorial *= n - i;
+    }
+    return factorial;
+  }
 }
--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,11 @@
 function checkSpam(str) {
-  // ваш код...
+  let lowerStr = str.toLowerCase();
+  const spamWords = ['1xBet', 'XXX'];
+  let result = false;
+  spamWords.forEach((spamWord) => {
+    if (lowerStr.includes(spamWord.toLowerCase())) {
+      result = true;
+    }
+  });
+  return result;  
 }
