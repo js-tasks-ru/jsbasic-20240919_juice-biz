df57f9a10f27ae6fde27a577779100d8a2c6dba8
Ivan Klyushin <juice_biz@mail.ru>
2024-10-10 13:34:48 +0300

    Fourth homework completed

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,7 @@
 function camelize(str) {
-  // ваш код...
+  return str.split('-')
+  .map(function(word, index) {
+    return index === 0 ? word : word[0].toUpperCase() + word.slice(1);
+  })
+  .join('');  
 }
