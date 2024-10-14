f7bf2a0c9f049e0e510d67c527a8e2d0a88ffe52
Ivan Klyushin <juice_biz@mail.ru>
2024-10-14 17:58:44 +0300

    Fifth homework completed

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,31 @@
 function highlight(table) {
-  // ваш код...
+  const tbody = table.querySelector('tbody');
+  for (let row of tbody.rows) {
+    let hasDataAvailable = true;
+    for (let cell of row.cells) {
+      if (cell.hasAttribute('data-available')) {
+        if (cell.getAttribute('data-available') === 'true') {
+          row.classList.add('available');
+        } else {
+          row.classList.add('unavailable');
+        }
+        hasDataAvailable = false;
+      } 
+    }    
+    if (hasDataAvailable) {
+      row.hidden = true;
+    }   
+    
+    const gender = row.cells[2].textContent;
+    if (gender === 'm') {
+      row.classList.add('male');
+    } else if (gender === 'f') {
+      row.classList.add('female');
+    }
+
+    const age = +row.cells[1].textContent;
+    if (age < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+  }
 }
